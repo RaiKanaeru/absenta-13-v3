be8eb5e2c072c2bf8f84196fcb8202743ff16f25
RaiKanaeru <raihanariansyah160307@gmail.com>
2025-12-24 10:36:33 +0700

    fix: add j.guru_id to GROUP BY in getSiswaJadwalRentang query

--- server/controllers/userInfoController.js
+++ server/controllers/userInfoController.js
@@ -359,7 +359,7 @@ export const getSiswaJadwalRentang = async (req, res) => {
                 AND ag2.tanggal = ?
                 AND ag2.guru_id = g2.id_guru
             WHERE j.kelas_id = ? AND j.hari = ?
-            GROUP BY j.id_jadwal, j.jam_ke, j.jam_mulai, j.jam_selesai, mp.nama_mapel, mp.kode_mapel, g.nama, g.nip, k.nama_kelas, ag.status, ag.keterangan, ag.waktu_catat, ag.ada_tugas, rk.kode_ruang, rk.nama_ruang, j.jenis_aktivitas, j.is_absenable, j.keterangan_khusus, j.is_multi_guru
+            GROUP BY j.id_jadwal, j.guru_id, j.jam_ke, j.jam_mulai, j.jam_selesai, mp.nama_mapel, mp.kode_mapel, g.nama, g.nip, k.nama_kelas, ag.status, ag.keterangan, ag.waktu_catat, ag.ada_tugas, rk.kode_ruang, rk.nama_ruang, j.jenis_aktivitas, j.is_absenable, j.keterangan_khusus, j.is_multi_guru
             ORDER BY j.jam_ke
         `, [targetDateStr, targetDateStr, kelasId, targetDay]);
 
